c1701e79e48c420d4fd3fd30faac6f1f1d8de5f2
SouvikChoudhury360 <choudhurysouvik2003@gmail.com>
2024-04-15 08:07:17 +0530

    create section, book added

---
+++ src/components/CreateSection.vue
@@ -0,0 +1,72 @@
+
+<template>
+  <div>
+    <div class="jumbotron text-center cyan">
+      <h2>Create Section</h2>
+      <router-link to="/admindashboard">See all already created sections here</router-link>
+    </div>
+    <div class="container box" style="text-align:center;">
+      <form>
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" class="form-control" name="name" v-model="venue.name">
+          <label for="address">Description:</label>
+          <input type="text" class="form-control" name="address" v-model="venue.address">
+        </div>
+        <button type="submit" class="btn btn-primary" @click="submitData">Create</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style>
+.box {
+  padding: 24px;
+  background-color: aliceblue;
+  border-radius: 24px;
+  border: 1px solid black;
+}
+.cyan {
+  background-color: aliceblue;
+}
+.body {
+  background-color: #f6f7ff;
+}
+</style>
+
+<script>
+const axios = require("axios");
+export default {
+  name: 'CreateSection',
+  data() {
+    return {
+      venue: {
+        address: null,
+        name: null,
+        capacity: null
+      }
+    };
+  },
+  methods: {
+    submitData(e) {
+      axios({
+        method: "post",
+        url: `https://ticketshow-api.onrender.com/createVenue`,
+        data: this.venue,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((result) => {
+          console.log(result.data);
+          let dashurl = '/admindashboard/';
+          this.$router.push({ path: dashurl });
+        })
+        .catch((err) => {
+          window.alert(err);
+        });
+      e.preventDefault();
+    },
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import UserLogin from '../components/UserLogin.vue'
 import UserSignup from '../components/UserSignup.vue'
 import AdminLogin from '../components/AdminLogin.vue'
 import UserDashboard from '../components/UserDashboard.vue'
+import CreateSection from '../components/CreateSection.vue'
+import CreateBook from '../components/CreateBook.vue'
 
 
 const routes = [
@@ -31,7 +33,17 @@ const routes = [
         path: '/dashboard/:userid',
         name: 'UserDashboard',
         component: UserDashboard
-      }
+      },
+      {
+        path: '/create/section',
+        name: 'CreateSection',
+        component: CreateSection
+      },
+      {
+        path: '/create/book/:sectionid',
+        name: 'CreateBook',
+        component: CreateBook
+      },
   ]
   
   const router = createRouter({
